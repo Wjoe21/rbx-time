2a6ce257d79dc076eb5f07b1f36d361795af0e3c
Wjoe21 <WasifJ21@hotmail.com>
2021-01-21 12:33:46 -0500

    added post for logging in

--- public/client.js
+++ public/client.js
@@ -1,7 +1,21 @@
 const btnTest = document.getElementById("buttonTest")
+const userInput = document.getElementById("userInput");
+const passInput = document.getElementById("passwordInput");
 
 btnTest.addEventListener("click", function(e){
     console.log("clicked");
+    const data = {username: userInput.value, password: passInput.value};
     btnTest.className = "button is-success is-loading";
     btnTest.setAttribute("disabled", true);
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log("success");
+    })
 })
